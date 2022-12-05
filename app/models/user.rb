362919d53bb0d4866ae8a8f788fa39335976bd79
PHILIP <philophilo037@gmail.com>
2022-12-05 23:39:48 +0300

    routes; request for signup which is a create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # We will need to use the has_secure_password macros to make sure that the password is hashed using bcrypt.
-    #  We also need to validate the username to make sure that all users have unique usernames on the model.
+    #  We also need to validate the full_name to make sure that all users have unique usernames on the model.
     has_secure_password
     validates :full_name, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-# Rails.application.routes.draw do
-#   # route our requests, for the signup which is a create action.
-#   namespace :api do
-#     namespace :v1 do
-#       resources :users, only: [:create]
-#     end
-#   end
-# end
+Rails.application.routes.draw do
+  # route our requests, for the signup which is a create action.
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+    end
+  end
+end
