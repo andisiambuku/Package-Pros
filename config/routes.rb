01c48cb1308c1ac5fb9a8c3a02bf415bb3d0f39b
andisiambuku <roselandambuku@gmail.com>
2022-12-05 09:11:22 +0300

    add: routes for parcel orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'parcels', to: 'parcel#index',
+  get 'parcels/:id', to: 'parcel#show',
+  post 'parcels', to: 'parcel#create',
+  delete 'parcels/:id', to: 'parcel#destroy',
+  put 'parcels/:id', to: 'parcel#update'
 
   # Defines the root path route ("/")
   # root "articles#index"
