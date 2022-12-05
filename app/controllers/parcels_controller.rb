839af3347b155fa293fca23397f4d520bf601f9d
andisiambuku <roselandambuku@gmail.com>
2022-12-05 09:17:48 +0300

    add: controller actions for parcel orders

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -1,3 +1,12 @@
 class ParcelsController < ApplicationController
-    
+    def  index 
+    end
+    def show 
+    end
+    def create 
+    end
+    def destroy 
+    end
+    def update
+    end
 end
