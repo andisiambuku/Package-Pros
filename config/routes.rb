bfd9237fac7d4f8dccd53902b434bca901db6a44
andisiambuku <roselandambuku@gmail.com>
2022-12-06 15:20:36 +0300

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/', to: 'parcels#index'
+  get 'parcels', to: 'parcels#index'
   get 'parcels/:id', to: 'parcels#show'
   post 'parcels', to: 'parcels#create'
   delete 'parcels/:id', to: 'parcels#destroy'
   put 'parcels/:id', to: 'parcels#update'
 
- 
-end
-=======
   # route our requests, for the signup which is a create action.
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
     end
   end
+
+ 
 end
->>>>>>> aa8b16dc6ebe2a87ed29687233d6f3ef1f6b7aca
+
+  
+  
+
+
