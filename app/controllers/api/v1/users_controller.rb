1fd63ae752005dec5a99a468413eba86b2253270
PHILIP <philophilo037@gmail.com>
2022-12-06 22:42:24 +0300

    add the encode_token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::UsersController < ApplicationController
     #create method in the users controller
     #creating a new user with the required params which are username, phone_number, password and email
+    #add the token which can be used to verify the user
     def create
         user = User.find_by(user_params)
         if user.valid?
+            token = encode_token(user_id: user.id)
             render json:{user: UserSerializer.new(user)}, status: :created
         else
             render json:{error: "Failed to create user"}, status: :unprocessable_entity
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    #add the encode_token method in the application_controller
+    def encode_token(payload)
+        JWT.encode(payload, 'my_s3cr3t')
+    end
 end
