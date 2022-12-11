3f084be0fa43cdcdf54543d7a021244dff4ae119
andisiambuku <roselandambuku@gmail.com>
2022-12-11 16:27:34 +0300

    deployment: removed railway config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
    #host configuration
-   config.hosts << "https://package-pros-production.up.railway.app/"
+  #  config.hosts << "https://package-pros-production.up.railway.app/"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
